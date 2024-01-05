c430aa563ec51fda7c6e298005732f1bdca7996d
Sten Remmelg <sten.remmelg@qualisys.se>
2024-01-05 18:05:08 +0200

    add timeseries loader

--- oura_analysis/loader.py
+++ oura_analysis/loader.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 import pandas as pd
 from pathlib import Path
+import numpy as np
+from typing import List, Dict
+import json
 
 
-class OuraData:
+class OuraDataNumeric:
     def __init__(self, data: pd.DataFrame) -> None:
         self._data = data
 
@@ -13,7 +16,33 @@ class OuraData:
         return self._data.copy()
 
     @staticmethod
-    def from_path(path: Path) -> OuraData:
+    def from_path(path: Path) -> OuraDataNumeric:
         """Loads data from a path"""
         data = pd.read_csv(path, header=0)
-        return OuraData(pd.read_csv(path))
+        return OuraDataNumeric(pd.read_csv(path))
+
+
+class OuraDataTimeseries:
+    def __init__(self, data: Dict) -> None:
+        self._data = data
+
+    @staticmethod
+    def from_path(path: Path) -> OuraDataTimeseries:
+        """loads data from path"""
+        with Path(path).open("r") as f:
+            data = json.load(f)
+        return OuraDataTimeseries(data)
+
+    def heart_rate(self) -> Dict[str, np.array]:
+        return {
+            item["date"]: np.array(item["heart_rate"]["items"]).astype(float)
+            for item in self._data["data"]
+            if item["heart_rate"] is not None
+        }
+
+    def hrv(self) -> Dict[str, np.array]:
+        return {
+            item["date"]: np.array(item["hrv"]["items"]).astype(float)
+            for item in self._data["data"]
+            if item["hrv"] is not None
+        }
--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,9 +1,18 @@
-from oura_analysis.loader import OuraData
+from oura_analysis.loader import OuraDataNumeric, OuraDataTimeseries
 
 
 def test_load_oura_data():
     """Tests that OuraData can be loaded from a path"""
-    data = OuraData.from_path("tests/oura_trends_for_test.csv")
+    data = OuraDataNumeric.from_path("tests/oura_trends_for_test.csv")
     data_table = data.data_table
     assert data_table.shape == (2, 54)
     assert "Average HRV" in data_table.columns
+
+
+def test_load_oura_data_from_json():
+    data = OuraDataTimeseries.from_path("tests/oura_timeseries_for_test.json")
+    hr = data.heart_rate()
+    hrv = data.hrv()
+    assert len(hr) == len(hrv)
+    assert len(hr) == 1
+    assert len(hr["2023-11-02"]) == 6
