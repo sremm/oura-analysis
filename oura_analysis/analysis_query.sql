077092fa4d3aba805e4ba8c56cbfa9cb057dc7eb
Sten Remmelg <sten.remmelg@gmail.com>
2025-10-23 20:33:27 +0300

    Add CTE for caffeine tags to make the query more readable.

--- oura_analysis/analysis_query.sql
+++ oura_analysis/analysis_query.sql
@@ -1,32 +1,34 @@
-CREATE OR REPLACE TABLE analysis_table AS WITH tags_day AS (
+CREATE OR REPLACE TABLE analysis_table AS -- CTE to filter caffeine-related tags
+    WITH caffeine_tags AS (
+        SELECT *
+        FROM tags
+        WHERE comment = 'Coffee'
+            OR tag_type_code IN ('tag_generic_coffee', 'tag_generic_caffeine')
+            OR (
+                tag_type_code = 'custom'
+                AND custom_name = 'Mate'
+            )
+    ),
+    tags_day AS (
         SELECT start_day,
-            /* Count all caffeine-related tags */
-            COUNT(*) FILTER (
-                WHERE comment = 'Coffee'
-                    OR tag_type_code IN ('tag_generic_coffee', 'tag_generic_caffeine')
-                    OR (
-                        tag_type_code = 'custom'
-                        AND custom_name = 'Mate'
-                    )
-            ) AS previous_day_caffeine_count,
-            MAX(
-                CASE
-                    WHEN comment = 'Coffee' THEN TRUE
-                    WHEN tag_type_code IN ('tag_generic_coffee', 'tag_generic_caffeine') THEN TRUE
-                    WHEN tag_type_code = 'custom'
-                    AND custom_name = 'Mate' THEN TRUE
-                    ELSE FALSE
-                END
-            ) AS previous_day_caffeine,
+            COUNT(*) AS previous_day_caffeine_count,
+            MAX(TRUE) AS previous_day_caffeine,
+            LIST(
+                EXTRACT(
+                    'hour'
+                    from start_time::timestamp
+                )
+            ) AS caffeine_hours,
             LIST(comment) AS all_comments,
             LIST(tag_type_code) AS all_tag_types
-        FROM tags
+        FROM caffeine_tags
         GROUP BY start_day
-    )
+    ) -- Final selection combining sleep scores with previous day's caffeine tags
 SELECT s.day AS sleep_date,
     s.score AS sleep_score,
     t.all_comments,
     t.all_tag_types,
+    t.caffeine_hours,
     COALESCE(t.previous_day_caffeine, FALSE) AS previous_day_caffeine,
     COALESCE(t.previous_day_caffeine_count, 0) AS previous_day_caffeine_count
 FROM sleep_score s
