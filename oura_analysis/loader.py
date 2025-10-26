755eb2b53cafab9c1c232f479564a6caf1409297
Sten Remmelg <sten.remmelg@gmail.com>
2025-10-26 12:47:45 +0200

    Refactor to prepare for next analysis.

--- oura_analysis/loader.py
+++ oura_analysis/loader.py
@@ -2,7 +2,11 @@ from __future__ import annotations
 
 from pathlib import Path
 
+import duckdb
 import pandas as pd
+from loguru import logger
+
+from oura_analysis.settings import DATA_FOLDER, REPO_ROOT
 
 
 class OuraData:
@@ -18,3 +22,53 @@ class OuraData:
     def from_path(path: Path) -> OuraData:
         """Loads data from a path"""
         return OuraData(pd.read_csv(path, sep=";", header=0))
+
+
+def load_query(name: str) -> str:
+    query_file = REPO_ROOT / name
+    with open(query_file, "r") as f:
+        return f.read()
+
+
+def duckdb_connect() -> duckdb.DuckDBPyConnection:
+    db_filename = DATA_FOLDER / "sleep_analysis_2020-05-01_2026-01-01.duckdb"
+
+    # as of now recreated each time when script is run
+    if db_filename.exists():
+        db_filename.unlink()
+
+    duckdb_conn = duckdb.connect(database=db_filename)
+    return duckdb_conn
+
+
+def load_base_data_into_db(duckdb_conn: duckdb.DuckDBPyConnection) -> tuple:
+    ## DATA LOADING
+    sleep_score_file = DATA_FOLDER / "sleep_score_data_2020-05-01_2026-01-01.json"
+    sleep_data_file = DATA_FOLDER / "sleep_data_2020-05-01_2026-01-01.json"
+    tags_file = DATA_FOLDER / "tags_data_2020-05-01_2026-01-01.json"
+
+    logger.info("Loading sleep data")
+    sleep_score_data = duckdb_conn.read_json(sleep_score_file)
+    sleep_data = duckdb_conn.read_json(sleep_data_file)
+    tags_data = duckdb_conn.read_json(tags_file)
+
+    ## DATA PREPARATION
+    logger.info("Creating tables in DuckDB")
+    duckdb_conn.execute(
+        "CREATE OR REPLACE TABLE sleep_score AS SELECT * FROM sleep_score_data"
+    )
+    duckdb_conn.execute("CREATE OR REPLACE TABLE sleep AS SELECT * FROM sleep_data")
+    duckdb_conn.execute("CREATE OR REPLACE TABLE tags AS SELECT * FROM tags_data")
+
+    logger.info("Inspecting tables")
+    logger.info(
+        f"Sleep table columns: {duckdb_conn.execute('DESCRIBE sleep_score').fetchall()}"
+    )
+    logger.info(
+        f"Tags table columns: {duckdb_conn.execute('DESCRIBE tags').fetchall()}"
+    )
+
+    # Add previous day to sleep table
+    logger.info("Adding previous_day column to sleep_score table")
+    duckdb_conn.execute("ALTER TABLE sleep_score ADD COLUMN previous_day DATE")
+    duckdb_conn.execute("UPDATE sleep_score SET previous_day = day - 1")
--- oura_analysis/step_2_analyse_caffeine.py
+++ oura_analysis/step_2_analyse_caffeine.py
@@ -4,63 +4,18 @@ import streamlit as st
 from loguru import logger
 from plotly import express as px
 
-from oura_analysis.settings import DATA_FOLDER, REPO_ROOT
+from oura_analysis.loader import duckdb_connect, load_base_data_into_db, load_query
+from oura_analysis.settings import DATA_FOLDER
 
 st.set_page_config(page_title="Oura Sleep Analysis", layout="wide")
 
 
-def load_query(name: str) -> str:
-    query_file = REPO_ROOT / name
-    with open(query_file, "r") as f:
-        return f.read()
-
-
-def duckdb_connect() -> duckdb.DuckDBPyConnection:
-    db_filename = DATA_FOLDER / "sleep_analysis_2020-05-01_2026-01-01.duckdb"
-
-    # as of now recreated each time when script is run
-    if db_filename.exists():
-        db_filename.unlink()
-
-    duckdb_conn = duckdb.connect(database=db_filename)
-    return duckdb_conn
-
-
 def load_data_and_prepare_data(duckdb_conn: duckdb.DuckDBPyConnection) -> tuple:
-    ## DATA LOADING
-    sleep_score_file = DATA_FOLDER / "sleep_score_data_2020-05-01_2026-01-01.json"
-    sleep_data_file = DATA_FOLDER / "sleep_data_2020-05-01_2026-01-01.json"
-    tags_file = DATA_FOLDER / "tags_data_2020-05-01_2026-01-01.json"
-
-    logger.info("Loading sleep data")
-    sleep_score_data = duckdb_conn.read_json(sleep_score_file)
-    sleep_data = duckdb_conn.read_json(sleep_data_file)
-    tags_data = duckdb_conn.read_json(tags_file)
-
-    ## DATA PREPARATION
-    logger.info("Creating tables in DuckDB")
-    duckdb_conn.execute(
-        "CREATE OR REPLACE TABLE sleep_score AS SELECT * FROM sleep_score_data"
-    )
-    duckdb_conn.execute("CREATE OR REPLACE TABLE sleep AS SELECT * FROM sleep_data")
-    duckdb_conn.execute("CREATE OR REPLACE TABLE tags AS SELECT * FROM tags_data")
-
-    logger.info("Inspecting tables")
-    logger.info(
-        f"Sleep table columns: {duckdb_conn.execute('DESCRIBE sleep_score').fetchall()}"
-    )
-    logger.info(
-        f"Tags table columns: {duckdb_conn.execute('DESCRIBE tags').fetchall()}"
-    )
-
-    # Add previous day to sleep table
-    logger.info("Adding previous_day column to sleep_score table")
-    duckdb_conn.execute("ALTER TABLE sleep_score ADD COLUMN previous_day DATE")
-    duckdb_conn.execute("UPDATE sleep_score SET previous_day = day - 1")
+    load_base_data_into_db(duckdb_conn)
 
     # Create a combined analyis table
     # join the tables on sleep_score.previous_day and tags.start_day
-    analysis_query = load_query("oura_analysis/analysis_query.sql")
+    analysis_query = load_query("oura_analysis/caffeine_analysis_query.sql")
     duckdb_conn.execute(analysis_query)
 
 
